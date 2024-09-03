dce1ba4be5d0d9e8ee8b502b7fbc9f2f75f40449
Thaynara Barcelos <vitoriathaynara63@gmail.com>
2024-09-03 17:03:42 -0300

    Resolvendo questoes

---
+++ atividade1/questão1.py
@@ -0,0 +1,6 @@
+idade = int (input ("Informe a sua idade: "))
+
+idadeM = (idade * 12)
+idadeD = (idade *365)
+
+print(f"Olá, sua idade em meses é {idadeM}, sua idade em dias é {idadeD}")
---
+++ atividade1/questão3.py
@@ -0,0 +1,7 @@
+nome = input ("Informe seu nome: ")
+fazenda = input ("Informe o nome da fazenda:")
+cavalos = int(input ("Informe a quantidade de carvalos que vivem na fazenda:")) 
+
+ferraduras = cavalos*4
+
+print(f" O proprietário(a) {nome},da fazenda {fazenda},possui {cavalos} cavalos, a quantidade total de ferraduras para equipar todos os cavalos é {ferraduras}")
